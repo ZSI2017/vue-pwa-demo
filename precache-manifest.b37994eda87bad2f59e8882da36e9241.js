19e0fb929341f72b57d1a9209cb2dc70b4d6598d
ZSI2017 <13407121461@163.com>
2019-06-16 00:04:14 +0800

    deploy

---
+++ precache-manifest.b37994eda87bad2f59e8882da36e9241.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35f50e7f91b0a1f40e6f",
+    "url": "/vue-pwa-demo/js/chunk-vendors.3e7c957a.js"
+  },
+  {
+    "revision": "ee2fffb6585bf4b6d6e7",
+    "url": "/vue-pwa-demo/js/app.83d56eb4.js"
+  },
+  {
+    "revision": "eff0b6802e1302f1a99400c1e31535b0",
+    "url": "/vue-pwa-demo/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-pwa-demo/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ee2fffb6585bf4b6d6e7",
+    "url": "/vue-pwa-demo/css/app.67dbbf33.css"
+  }
+];
